380518679dba4b8639e43a482e31d4ae73eeee21
gauravkadam935 <120292352+gauravkadam935@users.noreply.github.com>
2023-06-12 09:30:45 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,25 +1,110 @@
 
 import React, { Component } from "react";
+
 import '../styles/App.css';
 
+import data from '../data'
+
 class App extends Component {
-  constructor(props) {
-    super(props);
 
-    this.state = {
-      slides: props.slides,
-      index: 0,
-    };
-  }
+constructor(props) {
 
-  render() {
-    
-    const { index, slides } = this.state;
+super(props);
 
-    return (
-      <></>
-    );
-  }
+this.state = {
+
+slides: data,
+
+index: 0,
+
+};
+
+this.handleRestartClick = this.handleRestartClick.bind(this);
+
+this.handlePrevClick = this.handlePrevClick.bind(this);
+
+this.handleNextClick = this.handleNextClick.bind(this)
+
+}
+
+handleNextClick(){
+
+const { index, slides } = this.state;
+
+this.setState({index: index+1})
+
+}
+
+handlePrevClick(){
+
+const {index} = this.state;
+
+this.setState({index:index-1});
+
+}
+
+handleRestartClick(){
+
+this.setState({index:0})
+
+}
+
+render() {
+
+const { index, slides } = this.state;
+
+return (
+
+<div className="container">
+
+<div className="main">
+
+<button data-testid="button-next"
+
+disabled={index === slides.length - 1}
+
+onClick={this.handleNextClick}
+
+>Next</button>
+
+<button data-testid="button-prev"
+
+disabled={index === 0}
+
+onClick={this.handlePrevClick}
+
+>Prev</button>
+
+<button
+
+data-testid="button-restart"
+
+disabled={index === 0}
+
+onClick={this.handleRestartClick}
+
+>
+
+Restart
+
+</button>
+
+</div>
+
+<div className="all-slides">
+
+<h1 data- testid="title">{slides[index].title}</h1>
+
+<p data- testid="text">{slides[index].text}</p>
+
+</div>
+
+</div>
+
+);
+
+}
+
 }
 
 export default App;
